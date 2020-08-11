c3bdb807eb0e87cb42c96f3b51890566cc8922c8
JEROME Emilien <jerome.emi@onja.org>
2020-08-11 16:29:15 +0300

    added weekend projec correction

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const foods = [
 		price: 500,
 		title: 'Mofogasy',
 		spicy: false,
-		vegetarian: false,
+		vegetarian: true,
 	},
 ];
 
@@ -44,13 +44,100 @@ const totalElem = document.querySelector('.total');
 const spicy = document.querySelector('#spicy');
 const vegetarian = document.querySelector('#vegetarian');
 
-const loadFoodList = e => {};
+// Show the food element from the list
+const loadFoodList = () => {
+	// creat a copy of t5he initial array
+	let filteredFoods = [...foods];
 
-const addFoodToOrder = id => {};
+	// filter the spicy stuff if we want that
+	if(spicy.checked) {
+		filteredFoods = filteredFoods.filter(food => food.spicy);
+		console.log('We want the spicy staff');
+	}
+	// same for vagies
+	if (vegetarian.checked) {
+		filteredFoods = filteredFoods.filter(food => food.vegetarian);
+		console.log('We want the vegies');
+	}
+	const html = filteredFoods.map(food => {
+		return `
+		<li>
+			<span>
+				${food.title}
+				<img class="icon" ${food.spicy ? "" : "hidden"} src="./assets/flame.svg" alt="Spicy ${food.title}">
+				<img class="icon" ${food.vegetarian ? "" : "hidden"} src="./assets/leaf.svg" alt="Vegetarian ${food.title}">
+			</span>
+			<span>${food.price}</span>
+			<button value="${food.id}" class="add">Add</button>
+		</li>
+		`;
+	})
+	.join('');
+	foodList.innerHTML = html;
+};
 
-const handleListClick = e => {};
+// Add food element to the order
+const addFoodToOrder = id => {
+	console.log(id);
+	// find the food that has the same id
+	const newOrder = foods.find(food => food.id === id);
+	console.log(newOrder);
+	orders.push(newOrder);
 
-const showOrderList = () => {};
+	orderList.dispatchEvent(new CustomEvent('orderUpdated'));
+};
+
+// Event delegation to handle click on a food list button
+const handleListClick = e => {
+	if (e.target.matches('button.add')) {
+		addFoodToOrder(e.target.value);
+	}
+	// console.log(e);
+};
+
+/// show the order list, as we want it
+const showOrderList = () => {
+
+	// Create an object that count the number of times that each object is  
+	const instances = orders.reduce((acc, order) => {
+		if(acc[order.id]) {
+			acc[order.id]++;
+		} else {
+			acc[order.id] = 1;
+		}
+		return acc;
+	}, {});
+	 
+
+	// change the object into an array
+	const html = Object.entries(instances)
+	//loop through each properties of this array
+	.map(([foodId, numberOfFood]) => {
+		// const numberOfFood = order[1];
+
+		// get the full object back with its id
+		const fullOrder = foods.find(food => food.id === foodId);
+		return `
+		<li>
+			<span>${fullOrder.title}</span> 
+			<span>x ${numberOfFood}</span> 
+			<span>${fullOrder.price * numberOfFood} Ar</span>
+		</li>
+		`
+	}).join('');
+	orderList.innerHTML = html;
+	console.log(orders);
+	
+};
+// calculate the full bill
+const updateTotal = () => {
+	const total = orders.reduce((totalAcc, order) => {
+		return totalAcc + order.price;
+	}, 0);
+	totalElem.textContent = `${total} Ar`;
+};
+// custom event to update the total price
+orderList.addEventListener('orderUpdated', updateTotal);
 
 // ***** MODAL CODE *****
 
@@ -87,12 +174,18 @@ const handleEscape = e => {
 };
 
 // ******* LISTENERS *******
-
+// Modal listener
 orderButton.addEventListener('click', openModal);
 window.addEventListener('keydown', handleEscape);
 outerModal.addEventListener('click', handleClick);
+
+// event delegation on the food list
 foodList.addEventListener('click', handleListClick);
+
+// custom event for updating the order list
 orderList.addEventListener('orderUpdated', showOrderList);
+
+// listeners ofr our filters to reload
 spicy.addEventListener('change', loadFoodList);
 vegetarian.addEventListener('change', loadFoodList);
 window.addEventListener('DOMContentLoaded', loadFoodList);
